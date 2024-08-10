30079148f155ef967ee308fec6b0318545139a69
Vip-HackerZ <ommore2021107@gmail.com>
2024-08-11 03:56:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6592090338:AAEhbxEDPgmUcAyeck0L6WP-JITYYLPEMt4")
-    API_ID = int(os.environ["API_ID", 23291931]
-    API_HASH = os.environ["API_HASH", "4b11dd648188731fb7c9bc8083e8791c"]
-    AUTH_USERS = "6436809802"""
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ["API_ID", 18156248]
+    API_HASH = os.environ["API_HASH", "db946fb6805b1a698c679626b617e77a"]
+    AUTH_USERS = "2032347579"""
 
